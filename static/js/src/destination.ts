2f7062553e656ed214ff3008b2b0f25f97449d05
Cecilie Jadach Petersen <Jadach95@hotmail.com>
2026-03-10 20:33:07 +0100

    Added frontend validation for destination form

---
+++ static/js/src/destination.ts
@@ -0,0 +1,105 @@
+/////////////// DESTINATION TITLE VALIDATION ///////////////
+function addErrorTitle(errormessage:string):void {
+    const li = document.createElement("li");
+    li.textContent = errormessage;
+    document.getElementById("destination_title_errors").appendChild(li)
+}
+
+document.getElementById("formDestination").addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const destinationTitle = (document.getElementById("destination_title") as HTMLInputElement).value;
+
+    document.getElementById("destination_title_errors").textContent = '';
+
+    if (destinationTitle.trim() == "") {
+    addErrorTitle("Title must be filled out"); 
+    }
+    else if
+    (destinationTitle.length < 2) {
+    addErrorTitle("Title must be at least 2 characters");
+    }
+    else if 
+    (destinationTitle.length > 50) {
+        addErrorTitle("Title cannot exceed 50 characters");
+    }
+})
+
+/////////////// DESTINATION START DATE VALIDATION ///////////////
+function addErrorStartDate(errormessage:string):void {
+    const li = document.createElement("li");
+    li.textContent = errormessage;
+    document.getElementById("destination_start_date_errors").appendChild(li)
+}
+
+document.getElementById("formDestination").addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const destionationStartDate = (document.getElementById("destination_start_date") as HTMLInputElement).value;
+
+    document.getElementById("destination_start_date_errors").textContent = '';
+
+    if (destionationStartDate.trim() == "") {
+    addErrorStartDate("Start date must be filled out"); 
+    }
+})
+
+/////////////// DESTINATION END DATE VALIDATION ///////////////
+function addErrorEndDate(errormessage:string):void {
+    const li = document.createElement("li");
+    li.textContent = errormessage;
+    document.getElementById("destination_end_date_errors").appendChild(li)
+}
+
+document.getElementById("formDestination").addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const destionationEndDate = (document.getElementById("destination_end_date") as HTMLInputElement).value;
+
+    document.getElementById("destination_end_date_errors").textContent = '';
+
+    if (destionationEndDate.trim() == "") {
+    addErrorEndDate("End date must be filled out"); 
+    }
+})
+
+/////////////// DESTINATION LOCATION VALIDATION ///////////////
+function addErrorLocation(errormessage:string):void {
+    const li = document.createElement("li");
+    li.textContent = errormessage;
+    document.getElementById("destination_location_errors").appendChild(li)
+}
+
+document.getElementById("formDestination").addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const destinationLocation = (document.getElementById("destination_title") as HTMLInputElement).value;
+
+    document.getElementById("destination_location_errors").textContent = '';
+
+    if (destinationLocation.trim() == "") {
+    addErrorLocation("Location must be filled out"); 
+    }
+    else if
+    (destinationLocation.length < 2) {
+    addErrorLocation("Location must be at least 2 characters");
+    }
+    else if 
+    (destinationLocation.length > 50) {
+    addErrorLocation("Location cannot exceed 90 characters");
+    }
+})
+
+/////////////// DESTINATION COUNTRY VALIDATION ///////////////
+// function addErrorCountry(errormessage:string):void {
+//     const li = document.createElement("li");
+//     li.textContent = errormessage;
+//     document.getElementById("destination_country_errors").appendChild(li)
+// }
+
+// document.getElementById("formDestination").addEventListener("submit", (e: Event) => {
+//     e.preventDefault();
+//     const destinationCountry = (document.getElementById("user_password") as HTMLInputElement).value;
+
+//     document.getElementById("destination_country_errors").textContent = '';
+
+//     if (destinationCountry.trim() == "") {
+//     addPasswordErrorSignUp("Country must be filled out");
+// }
+// })
